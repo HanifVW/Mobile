40ba0b2250c39e2fae5a9c44d6b4b302598e696f
HanifVW <ianhanif66@gmail.com>
2020-03-18 18:23:30 +0700

    initial commit

--- app/src/main/java/com/example/prak2/Welcome.java
+++ app/src/main/java/com/example/prak2/Welcome.java
@@ -2,13 +2,25 @@ package com.example.prak2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
-
+Button setRe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+       setRe = (Button) findViewById(R.id.setR);
+       setRe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this,setReminder.class);
+                Welcome.this.startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/prak2/setReminder.java
+++ app/src/main/java/com/example/prak2/setReminder.java
@@ -3,13 +3,14 @@ package com.example.prak2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class Register extends AppCompatActivity {
+public class setReminder extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
+        setContentView(R.layout.activity_set_reminder);
     }
 }
